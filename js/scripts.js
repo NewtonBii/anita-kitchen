0e9bda31bbad3ebfbc4c5c686d701c87ae985e5c
Newton Bii <biinewton382@gmail.com>
2017-10-20 09:18:59 +0300

    Add scripts.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+<script>
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
