4cb98e08f3b2ab6bf333df1872b6a314ec0a41b4
Newton Bii <biinewton382@gmail.com>
2017-10-21 13:37:10 +0300

    Merge jscript changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-$(".clickable-ff").click(function() {
-  $(".fasfood-images").show("");
-  $(".fasfood-images").hide();
+function validateEmail() {
+
+
+var email = document.getElementById("mail");
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("You have entered an invalid email address!");
+  } else {
+    alert("Congratulations!")
+  }
 });
-$(".clickable-ff").click(function(){
-  $(".fastfood-images").toggle();
-  $(".fastfood-images").toggle();
-});
+}
